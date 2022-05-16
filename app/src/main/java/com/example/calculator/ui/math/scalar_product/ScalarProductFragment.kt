a76813372d05d927f03942ff460ec3e0e16aef3a
Okarisu <okarisu@protonmail.com>
2022-05-16 17:13:27 +0200

    přidán skalární součin, refaktor.

--- app/src/main/java/com/example/calculator/ui/math/scalar_product/ScalarProductFragment.kt
+++ app/src/main/java/com/example/calculator/ui/math/scalar_product/ScalarProductFragment.kt
@@ -1,17 +1,16 @@
-package com.example.calculator.ui.vector_product
+package com.example.calculator.ui.math.scalar_product
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.calculator.databinding.FragmentVectorProductBinding
+import com.example.calculator.databinding.FragmentScalarProductBinding
 
-import math.objects.Vector3
 
-class VectorProductFragment : Fragment() {
+class ScalarProductFragment : Fragment() {
 
-    private var _binding: FragmentVectorProductBinding? = null
+    private var _binding: FragmentScalarProductBinding? = null
 
     private val binding get() = _binding!!
 
@@ -21,14 +20,14 @@ class VectorProductFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentVectorProductBinding.inflate(inflater, container, false)
-        binding.calculateButton.setOnClickListener { computeVector() }
+        _binding = FragmentScalarProductBinding.inflate(inflater, container, false)
+
+        binding.calculateButton.setOnClickListener { computeScalarProduct() }
 
         return binding.root
     }
 
-    private fun computeVector() {
-
+    private fun computeScalarProduct() {
         val x1 = binding.vector1X.text.toString().toDoubleOrNull();
         val y1 = binding.vector1Y.text.toString().toDoubleOrNull();
         var z1 = binding.vector1Z.text.toString().toDoubleOrNull();
@@ -38,25 +37,25 @@ class VectorProductFragment : Fragment() {
         var z2 = binding.vector2Z.text.toString().toDoubleOrNull();
 
 
-        if((x1 == null) || (x2 == null) || (y1 == null) || (y2 == null)){
+        if ((x1 == null) || (x2 == null) || (y1 == null) || (y2 == null)) {
             binding.result.text = ""
             return
         } else {
 
-            if((z1 == null) || (z2 == null)){
+            if ((z1 == null) || (z2 == null)) {
                 z1 = 0.0
                 z2 = 0.0
             }
-            val result: Vector3 = Vector3(y1 * z2 - z1 * y2, z1 * x2 - x1 * z2, x1 * y2 - y2 * x1);
 
-            val resultString = "(" + result.x + "; " + result.y + "; " + result.z + ")";
+            val result = x1 * x2 + y1 * y2 + z1 * z2
 
-            binding.result.text = resultString
+            binding.result.text = result.toString()
+
         }
     }
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
