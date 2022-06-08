fcc0c2188e89ea169e7093cc61a19616a6606acd
Okarisu <okarisu@protonmail.com>
2022-06-08 18:49:17 +0200

    hotovy 3 fce

--- app/src/main/java/com/example/calculator/ui/vectors/vectors_uhel_vektoru.kt
+++ app/src/main/java/com/example/calculator/ui/vectors/vectors_uhel_vektoru.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import com.example.calculator.databinding.FragmentVectorsAngleBinding
 import com.example.calculator.math.functions.ScalarProduct
 import com.example.calculator.math.functions.VectorMagnitude
+import kotlin.math.PI
+import kotlin.math.acos
 import kotlin.math.cos
 import kotlin.math.pow
 
@@ -18,6 +20,7 @@ class vectors_uhel_vektoru : Fragment() {
 
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,12 +29,21 @@ class vectors_uhel_vektoru : Fragment() {
 
         _binding = FragmentVectorsAngleBinding.inflate(inflater, container, false)
 
-        binding.calculateButton.setOnClickListener { binding.result.text = computeVectorAngle() }
+        binding.calculateButton.setOnClickListener {
+
+            val result = computeVectorAngle()
+            if (result != null) {
+                binding.resultDeg.text = ConvertToDeg(result).toString() + "°"
+                binding.resultRad.text = result.toString() +  "π rad"
+
+            }
+        }
 
         return binding.root
     }
 
-    private fun computeVectorAngle(): String {
+    private fun computeVectorAngle(): Double? {
+
         val x1 = binding.vector1X.text.toString().toDoubleOrNull()
         val y1 = binding.vector1Y.text.toString().toDoubleOrNull()
         val z1 = binding.vector1Z.text.toString().toDoubleOrNull()
@@ -44,21 +56,26 @@ class vectors_uhel_vektoru : Fragment() {
         val nomenator1 = VectorMagnitude().GetVectorMagnitude(x1, y1, z1)
         val nomenator2 = VectorMagnitude().GetVectorMagnitude(x2, y2, z2)
 
-        var result: String
+        var result: Double
 
-        if(numerator == null || nomenator1 == null || nomenator2 == null){
-            return ""
+        return if (numerator == null || nomenator1 == null || nomenator2 == null) {
+            null
         } else {
             val fraction: Double = numerator.div((nomenator1 * nomenator2))
-            val angle = cos(fraction).pow(-1)
-            return angle.toString()
+            val angle = acos(fraction)
+            angle
         }
     }
 
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+private fun ConvertToDeg(angle: Double): Double {
+    return angle * 180 / PI
+}
+
+
+override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+}
 
 }
