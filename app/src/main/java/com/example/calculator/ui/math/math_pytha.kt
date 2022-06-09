3837392968dae22ef3f39cd12a618367679772f7
Okarisu <okarisu@protonmail.com>
2022-06-09 09:21:34 +0200

    backup

--- app/src/main/java/com/example/calculator/ui/math/math_pytha.kt
+++ app/src/main/java/com/example/calculator/ui/math/math_pytha.kt
@@ -1,12 +1,16 @@
 package com.example.calculator.ui.math
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.calculator.databinding.FragmentPythaBinding
-import com.example.calculator.math.functions.ScalarProduct
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 
 class math_pytha : Fragment() {
@@ -14,6 +18,7 @@ class math_pytha : Fragment() {
     private var _binding: FragmentPythaBinding? = null
 
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,21 +28,49 @@ class math_pytha : Fragment() {
 
         _binding = FragmentPythaBinding.inflate(inflater, container, false)
 
-        binding.calculateButton.setOnClickListener { computeScalarProduct() }
+        binding.calculateButton.setOnClickListener { Pytha() }
 
         return binding.root
     }
 
-    private fun computeScalarProduct() {
-        val x1 = binding.vector1X.text.toString().toDoubleOrNull()
+    private fun Pytha() {
+        val a = binding.valA.text.toString().toDoubleOrNull()
+        val b = binding.valB.text.toString().toDoubleOrNull()
+        val c = binding.valC.text.toString().toDoubleOrNull()
 
-        binding.result.text = result
+        if ((a == null && b == null) || (a == null && c == null) || (b == null && c == null)) {
+            null
+        } else {
+
+            binding.result.text = "42"
+        }
+    }
+
+    private fun ComputePytha(a: Double, b: Double, c: Double): Double? {
+
+
+           return if (a == null) {
+                val aPow = c.pow(2) - b.pow(2)
+                if (aPow < 0)
+                    null
+                else
+                    sqrt(aPow)
+            } else if (b == null) {
+                val bPow = c.pow(2) - a.pow(2)
+                if (bPow < 0)
+                    null
+                else
+                    sqrt(bPow)
+            } else {
+                sqrt(a.pow(2) + b.pow(2))
+            }
+
     }
 
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
 
-}
+    }
